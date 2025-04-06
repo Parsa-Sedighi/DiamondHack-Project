86a60364d078b95b89ee85c9945748176020b5b4
Parsa Sedighi <parsasedighi2004@icloud.com>
2025-04-05 22:52:36 -0700

    Updated eveything

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def get_joke():
     # Call the public joke API
     response = requests.get("https://official-joke-api.appspot.com/random_joke")
-    
-    # Convert the response into JSON format
+#    
+#    # Convert the response into JSON format
     joke_data = response.json()
-
-    # Extract setup and punchline
+#
+#    # Extract setup and punchline
     setup = joke_data['setup']
     punchline = joke_data['punchline']
-
-    # Pass the joke to the HTML template
+#
+#    # Pass the joke to the HTML template
     return render_template('joke.html', setup=setup, punchline=punchline)
 
 if __name__ == '__main__':
