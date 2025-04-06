4732f5108976122f3a22c82dd5a61dbc3e45048c
Parsa Sedighi <parsasedighi2004@icloud.com>
2025-04-06 03:09:35 -0700

    commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+from ManimTest.manimations import manimCaller
+ 
+def main():
+     print("Hello from manimations!")
+     
+     manimCaller.manimLinearEq(1, 2, 3)
+ 
+ 
+ 
+if __name__ == "__main__":
+     main()
